0567729db29158069cbeee8dda7ebc6f41f7bd00
kennankole <kennankole@gmail.com>
2022-09-12 12:32:28 +0300

    Fixing linters

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,9 +1,4 @@
 function myFunction(x){
-    let logo = document.querySelector('nav');
     x.classList.toggle("change");
-    
-    
-
 }
-let menu = document.querySelector('.right');
-menu.classList.v
+
