d881ab883765fccefd48279d4f54eb98a0036243
kennankole <kennankole@gmail.com>
2022-09-12 20:32:52 +0300

    Fixed linter errors.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,8 +2,7 @@ function myFunction(x){
     let show = document.querySelector('.right');
     x.classList.toggle("change");
     show.classList.toggle('show-menu');
-    
-
+    show.classList.toggle('show-menu>a');
 }
 
 const humberger = document.querySelector(".hambuger-menu");
@@ -12,10 +11,4 @@ const navMenu = document.querySelector(".right");
 document.querySelectorAll(".right").forEach(e => e.addEventListener("click", () => {
     humberger.classList.remove("change");
     navMenu.classList.remove("show-menu");
-}))
-
-
-
-
-
-
+}));
