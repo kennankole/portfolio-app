62da65eaa55fd4e05f7ce6edea09b5aa5d467872
kennankole <kennankole@gmail.com>
2022-09-12 12:30:28 +0300

    Added css and Js to transform humberger menu on click.

---
+++ app/static/js/index.js
@@ -0,0 +1,9 @@
+function myFunction(x){
+    let logo = document.querySelector('nav');
+    x.classList.toggle("change");
+    
+    
+
+}
+let menu = document.querySelector('.right');
+menu.classList.v
