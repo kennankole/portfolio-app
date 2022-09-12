e5b52ce027e7d2cc9e38ebec29d7629f5f16feae
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-09-12 12:35:36 +0100

    add funtion to menu button

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,4 +1,18 @@
 function myFunction(x){
+    let show = document.querySelector('.right');
     x.classList.toggle("change");
+    show.classList.toggle('show-menu');
+    
+
 }
+
+function closeMenuItem(){
+    let menuItem = document.querySelector('.menu-item');
+    let show = document.querySelector('.right');
+    myFunction(show);
+}
+
+
+
+
 
